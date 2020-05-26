82bbe821df7ae2212a7bd2c7d0caea419d7a9a4c
krozic <kyle.rozic@gmail.com>
2020-05-26 15:02:24 -0400

    test

--- Project/run_analysis.R
+++ Project/run_analysis.R
@@ -1,3 +1,4 @@
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 download.file(fileUrl, "./data/UCI_HAR.zip", method = "curl")
+
